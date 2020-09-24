67aef6e407a8e75e217dfb4bb1dfd110a24ed456
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-09-24 11:21:20 +0300

    AE-516: Fix purgecss in build

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,11 @@
 const postcssPresetEnv = require('postcss-preset-env');
 const cssnano = require('cssnano');
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./src/**/*.svelte'],
-  whitelistPatterns: [/svelte-/],
-
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-});
-
 const production = process.env.NODE_ENV === 'production';
 
 module.exports = {
   plugins: [
     require('tailwindcss'),
     postcssPresetEnv(),
-    ...(production
-      ? [(purgecss, cssnano(['default', { autoprefix: true }]))]
-      : [])
+    ...(production ? [cssnano('default', { autoprefix: true })] : [])
   ]
 };
