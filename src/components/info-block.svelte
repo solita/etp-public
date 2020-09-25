1e322b521ed2e66513d98a783fe166241d6d5185
Pasi Semykine <23694709+pasisem@users.noreply.github.com>
2020-09-25 16:27:54 +0300

    Add InfoBlock component

---
+++ src/components/info-block.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let title;
+  export let text;
+</script>
+
+<style>
+</style>
+
+<section class="px-16 flex flex-col justify-center items-center bg-beige py-8">
+  <h2 class="font-bold text-ashblue text-xl tracking-widest mb-4">{title}</h2>
+  <div class="flex items-center">
+    <span class="font-icon text-6xl mr-4 text-ashblue">info</span>
+    <p class="font-medium">{text}</p>
+  </div>
+</section>
