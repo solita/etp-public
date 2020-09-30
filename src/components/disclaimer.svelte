dd124fbb13498dcd237fe2c52f3afa46ae75d4c4
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-09-30 12:01:40 +0300

    AE-558: Add disclaimer

---
+++ src/components/disclaimer.svelte
@@ -0,0 +1,10 @@
+<div class="flex flex-col items-center text-white py-4">
+  <strong>KEHITYSVERSIO</strong>
+  <p>
+    <slot />
+    Virallisen, käytössä olevan palvelun, löydät osoitteesta
+    <a href="https://www.energiatodistusrekisteri.fi">
+      www.energiatodistusrekisteri.fi
+    </a>
+  </p>
+</div>
