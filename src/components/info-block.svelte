dc747a8dc4e09d8a94a0f377d54bd91f4d68304b
Pasi Semykine <23694709+pasisem@users.noreply.github.com>
2020-10-01 17:04:36 +0300

    Add responsiveness, Update icon to info-block

--- src/components/info-block.svelte
+++ src/components/info-block.svelte
@@ -1,16 +1,30 @@
 <script>
+  import IconInfo from '@Asset/icons/info.svg';
+
   export let title;
 </script>
 
 <style>
+  .icon {
+    width: auto;
+    height: 4em;
+  }
+
+  .icon.smaller {
+    height: 2.5em;
+  }
 </style>
 
-<section class="px-16 flex flex-col justify-center items-center bg-beige py-8">
-  <h2 class="font-bold text-ashblue text-xl tracking-widest mb-4">{title}</h2>
+<section
+  class="flex flex-col justify-center items-left bg-beige sm:px-16 sm:py-8 px-4 py-4">
+  <img class="icon smaller sm:hidden mr-4 " src={IconInfo} alt="Info icon" />
+  <h2 class="font-bold text-ashblue text-xl tracking-widest mb-4 text-left">
+    {title}
+  </h2>
   <div class="flex items-center">
-    <span class="font-icon text-6xl mr-4 text-ashblue">info</span>
+    <img class="icon mr-4 hidden sm:block" src={IconInfo} alt="Info icon" />
     <p class="font-medium">
-      <slot></slot>
+      <slot />
     </p>
   </div>
 </section>
