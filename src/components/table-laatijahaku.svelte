4921abae575672744404c0fce619827703424bc5
Pasi Semykine <23694709+pasisem@users.noreply.github.com>
2020-10-02 17:31:38 +0300

    Add laatijahaku page, table responsiveness

--- src/components/table-laatijahaku.svelte
+++ src/components/table-laatijahaku.svelte
@@ -39,41 +39,41 @@
   }
 </style>
 
-<div>
-  <div class="flex">
-    <div class="flex items-center space-x-1 mr-3">
-      <input id="perustaso" type="checkbox" />
-      <label for="perustaso">Perustaso</label>
-      <div class="icon-container relative">
-        <img class="icon" src={IconInfo} alt="Info icon" />
-        <div class="info-popup">
-          <strong>Perustaso:</strong>
-          <p>
-            Voi laatia energiatodistuksen rakennukselle tai rakennuksen osalle,
-            jossa laskennallisen kokonaisenergiankulutuksen laskemiseen
-            käytetään kuukausitason laskentamenetelmää. Tyypillinen tällainen
-            rakennus on esimerkiksi omakotitalo.
-          </p>
-        </div>
-      </div>
-    </div>
-
-    <div class="flex items-center space-x-1">
-      <input id="ylempitaso" type="checkbox" />
-      <label for="ylempitaso">Ylempi taso</label>
-      <div class="icon-container relative">
-        <img class="icon" src={IconInfo} alt="Info icon" />
-        <div class="info-popup">
-          <strong>Ylempi taso:</strong>
-          <p>???</p>
-        </div>
+<div class="flex">
+  <div class="flex items-center space-x-1 mr-3">
+    <input id="perustaso" type="checkbox" />
+    <label for="perustaso">Perustaso</label>
+    <div class="icon-container relative">
+      <img class="icon" src={IconInfo} alt="Info icon" />
+      <div class="info-popup">
+        <strong>Perustaso:</strong>
+        <p>
+          Voi laatia energiatodistuksen rakennukselle tai rakennuksen osalle,
+          jossa laskennallisen kokonaisenergiankulutuksen laskemiseen käytetään
+          kuukausitason laskentamenetelmää. Tyypillinen tällainen rakennus on
+          esimerkiksi omakotitalo.
+        </p>
       </div>
     </div>
   </div>
-  {#if laatijat.length < 1}
-    <span>Ei tuloksia.</span>
-  {:else}
-    <table class="table-auto text-left my-2">
+
+  <div class="flex items-center space-x-1">
+    <input id="ylempitaso" type="checkbox" />
+    <label for="ylempitaso">Ylempi taso</label>
+    <div class="icon-container relative">
+      <img class="icon" src={IconInfo} alt="Info icon" />
+      <div class="info-popup">
+        <strong>Ylempi taso:</strong>
+        <p>???</p>
+      </div>
+    </div>
+  </div>
+</div>
+{#if laatijat.length < 1}
+  <span>Ei tuloksia.</span>
+{:else}
+  <div class="w-full overflow-x-scroll lg:overflow-auto">
+    <table class="w-full table-auto text-left my-2">
       <thead>
         <tr>
           <th>Nimi</th>
@@ -88,24 +88,24 @@
       <tbody>
         {#each laatijat as laatija}
           <tr>
-            <td>{laatija.nimi}</td>
-            <td>{laatija.patevyys}</td>
-            <td>{laatija.alue}</td>
-            <td>{laatija.postinum}</td>
-            <td>
+            <td data-title="Nimi">{laatija.nimi}</td>
+            <td data-title="Pätevyys">{laatija.patevyys}</td>
+            <td data-title="Päätoiminta">{laatija.alue}</td>
+            <td data-title="Postitoimipaikka">{laatija.postinum}</td>
+            <td data-title="WWW">
               <a href={laatija.link} title={laatija.link}>
                 <img
-                  class="icon mx-auto"
+                  class="icon md:mx-auto"
                   src={IconWeb}
                   alt="Website link icon" />
               </a>
             </td>
-            <td>
+            <td data-title="Email">
               <a href="mailto:{laatija.email}" title={laatija.email}>
-                <img class="icon mx-auto" src={IconMail} alt="Email icon" />
+                <img class="icon md:mx-auto" src={IconMail} alt="Email icon" />
               </a>
             </td>
-            <td class="flex flex-no-wrap">
+            <td data-title="Puhelinnumero" class="flex flex-no-wrap">
               <img class="icon smaller mr-1" src={IconPhone} alt="Phone icon" />
               <span>{laatija.puh}</span>
             </td>
@@ -113,22 +113,22 @@
         {/each}
       </tbody>
     </table>
-    <div class="flex w-full justify-start my-3">
-      <div class="counter">Tuloksia {laatijat.length}</div>
-      <div
-        class="pagination flex self-center mx-auto text-green text-center font-semibold">
-        <span class="cursor-pointer uppercase mr-2">edellinen</span>
-        <div class="flex pages">
-          <span class="cursor-pointer uppercase px-1">1</span>
-          <span class="cursor-pointer uppercase px-1">2</span>
-          <span class="cursor-pointer uppercase px-1">3</span>
-          <span class="px-2">-</span>
-          <span class="cursor-pointer uppercase px-1">18</span>
-          <span class="cursor-pointer uppercase px-1">19</span>
-          <span class="cursor-pointer uppercase px-1">20</span>
-        </div>
-        <span class="cursor-pointer uppercase ml-2">seuraava</span>
+  </div>
+  <div class="flex w-full justify-start my-3">
+    <div class="counter">Tuloksia {laatijat.length}</div>
+    <div
+      class="pagination flex self-center mx-auto text-green text-center font-semibold">
+      <span class="cursor-pointer uppercase mr-2">edellinen</span>
+      <div class="flex pages">
+        <span class="cursor-pointer uppercase px-1">1</span>
+        <span class="cursor-pointer uppercase px-1">2</span>
+        <span class="cursor-pointer uppercase px-1">3</span>
+        <span class="px-2">-</span>
+        <span class="cursor-pointer uppercase px-1">18</span>
+        <span class="cursor-pointer uppercase px-1">19</span>
+        <span class="cursor-pointer uppercase px-1">20</span>
       </div>
+      <span class="cursor-pointer uppercase ml-2">seuraava</span>
     </div>
-  {/if}
-</div>
+  </div>
+{/if}
