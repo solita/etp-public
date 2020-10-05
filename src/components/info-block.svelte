7697aeb456e83c57ecf041a3ffb97ddcaac59245
Pasi Semykine <23694709+pasisem@users.noreply.github.com>
2020-10-05 15:38:49 +0300

    Add min-height to info-block

--- src/components/info-block.svelte
+++ src/components/info-block.svelte
@@ -5,6 +5,9 @@
 </script>
 
 <style>
+  section{
+    min-height: 12rem;
+  }
   .icon {
     width: auto;
     height: 4em;
@@ -17,7 +20,7 @@
 
 <section
   class="flex flex-col justify-center items-left bg-beige sm:px-16 sm:py-8 px-4 py-4">
-  <img class="icon smaller sm:hidden mr-4 " src={IconInfo} alt="Info icon" />
+  <img class="icon smaller sm:hidden mr-4" src={IconInfo} alt="Info icon" />
   <h2 class="font-bold text-ashblue text-xl tracking-widest mb-4 text-left">
     {title}
   </h2>
