894d87a2b317547edad79be11f96cdab8cf6a65b
Pasi Semykine <23694709+pasisem@users.noreply.github.com>
2020-10-09 18:56:45 +0300

    Add Footer component with layout fixes

--- src/app.svelte
+++ src/app.svelte
@@ -3,9 +3,9 @@
   import { _ } from '@Localization/localization';
   import Router from '@Router/router';
 
-  import Button, { styles as buttonStyles } from '@Component/button';
   import Container, { styles as containerStyles } from '@Component/container';
   import Disclaimer from '@Component/disclaimer';
+  import Footer from '@Component/footer';
 </script>
 
 <style>
@@ -31,63 +31,7 @@
     <Router />
   </main>
 
-  <Container {...containerStyles.grey}>
-    <footer class="flex flex-col bg-grey px-2 sm:px-16 py-4 sm:py-16">
-      <article class="flex flex-col md:flex-row -mx-4 -my-4 md:my-0">
-        <section class="md:w-1/3 px-4 py-4 md:py-0">
-          <h2 class="mb-4">
-            Energiatodistukset sekä niiden käyttö perustuu lakiin ja sitä
-            valvotaan
-          </h2>
-          <p>
-            Asumisen rahoitus- ja kehittämiskeskus (ARA) on valtion
-            viranomainen, joka valvoo todistusten käyttöä ja oikeellisuutta.
-            Vastaamme myös tämän palvelun ylläpidosta. Palvelua ja valvontaa
-            hoitavat rautaiset energia-ammattilaisemme. Jos sinulla on
-            kysyttävää, otathan yhteyttä!
-          </p>
-          <a href="/" class="underline text-green">Siirry ARA.fi</a>
-        </section>
-        <section
-          class="md:w-1/3 flex flex-col justify-between px-4 py-4 md:py-0">
-          <h2 class="mb-4">Ota yhteyttä</h2>
-          <address class="not-italic">
-            <span>Sähköposti:</span>
-            <a href="/" class="underline text-green">energiatodistus@ara.fi</a>
-          </address>
-          <address class="not-italic">
-            <ol>
-              <li>Asumisen rahoitus- ja kehittämiskeskus</li>
-              <li>PL 30</li>
-              <li>15141</li>
-              <li>LAHTI</li>
-            </ol>
-          </address>
-          <address class="not-italic">
-            <span>Puhelinvaihde:</span>
-            <a href="/" class="underline text-green">029 525 0800</a>
-          </address>
-          <a href="/" class="block underline text-green">Tietoa sivustosta</a>
-        </section>
-        <div class="md:w-1/3 flex flex-col justify px-4 -my-4 py-4 md:py-0">
-          <section class="py-4">
-            <h2 class="mb-4">Laatijoiden pavelu</h2>
-            <Button {...buttonStyles.ashblue}>Kirjaudu</Button>
-          </section>
-          <section class="py-4">
-            <h2 class="mb-4">palaute on aina tervetullutta</h2>
-            <Button {...buttonStyles.green}>Lähetä palautetta</Button>
-          </section>
-        </div>
-      </article>
-
-      <div class="my-8 border-t-2 border-gray-400">
-        <img
-          src="images/ara-logo.png"
-          alt="ARA - asumisen rakennus- ja kehityskeskus" />
-      </div>
-    </footer>
-  </Container>
+  <Footer />
 
   <div class="sticky bottom-0">
     <Container {...containerStyles.red}>
---
+++ src/components/footer.svelte
@@ -0,0 +1,60 @@
+<script>
+  import Button, { styles as buttonStyles } from '@Component/button';
+  import Container, { styles as containerStyles } from '@Component/container';
+</script>
+
+<Container {...containerStyles.grey}>
+  <footer class="flex flex-col bg-grey px-2 sm:px-16 py-4 sm:py-16">
+    <article class="flex flex-col md:flex-row md:my-0 md:space-x-4">
+      <section class="md:w-1/3 py-4 md:py-0">
+        <h2 class="mb-4">
+          Energiatodistukset sekä niiden käyttö perustuu lakiin ja sitä
+          valvotaan
+        </h2>
+        <p>
+          Asumisen rahoitus- ja kehittämiskeskus (ARA) on valtion viranomainen,
+          joka valvoo todistusten käyttöä ja oikeellisuutta. Vastaamme myös
+          tämän palvelun ylläpidosta. Palvelua ja valvontaa hoitavat rautaiset
+          energia-ammattilaisemme. Jos sinulla on kysyttävää, otathan yhteyttä!
+        </p>
+        <a href="/" class="underline text-green">Siirry ARA.fi</a>
+      </section>
+      <section class="md:w-1/3 flex flex-col justify-between py-4 md:py-0">
+        <h2 class="mb-4">Ota yhteyttä</h2>
+        <address class="not-italic">
+          <span>Sähköposti:</span>
+          <a href="/" class="underline text-green">energiatodistus@ara.fi</a>
+        </address>
+        <address class="not-italic">
+          <ol>
+            <li>Asumisen rahoitus- ja kehittämiskeskus</li>
+            <li>PL 30</li>
+            <li>15141</li>
+            <li>LAHTI</li>
+          </ol>
+        </address>
+        <address class="not-italic">
+          <span>Puhelinvaihde:</span>
+          <a href="/" class="underline text-green">029 525 0800</a>
+        </address>
+        <a href="/" class="block underline text-green">Tietoa sivustosta</a>
+      </section>
+      <div class="md:w-1/3 flex flex-col py-4 md:py-0">
+        <section>
+          <h2 class="mb-4">Laatijoiden pavelu</h2>
+          <Button {...buttonStyles.ashblue}>Kirjaudu</Button>
+        </section>
+        <section class="py-4">
+          <h2 class="mb-4">palaute on aina tervetullutta</h2>
+          <Button {...buttonStyles.green}>Lähetä palautetta</Button>
+        </section>
+      </div>
+    </article>
+
+    <div class="my-8 border-t-2 border-gray-400">
+      <img
+        src="images/ara-logo.png"
+        alt="ARA - asumisen rakennus- ja kehityskeskus" />
+    </div>
+  </footer>
+</Container>
