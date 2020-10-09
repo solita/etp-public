ac1d8aa896ddc632ea7d8500422ba0ece9e7c578
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-10-09 12:25:55 +0300

    AE-449: Add readable stores for laatijahaku values

--- src/api/geo-api.js
+++ src/api/geo-api.js
@@ -3,10 +3,11 @@ const baseUrl = '/api/public';
 const toimintaalueetUrl = `${baseUrl}/toimintaalueet/`;
 const countriesUrl = `${baseUrl}/toimintaalueet/`;
 const postinumerotUrl = `${baseUrl}/postinumerot`;
+const kunnatUrl = `${baseUrl}/kunnat`;
 
-export const toimintaalueet = fetch =>
-  fetch(toimintaalueetUrl).then(response => response.json());
-export const countries = fetch =>
-  fetch(countriesUrl).then(response => response.json());
-export const postinumerot = fetch =>
-  fetch(postinumerotUrl).then(response => response.json());
+const fetchJson = (fetch, url) => fetch(url).then(response => response.json());
+
+export const toimintaalueet = fetch => fetchJson(fetch, toimintaalueetUrl);
+export const countries = fetch => fetchJson(fetch, countriesUrl);
+export const postinumerot = fetch => fetchJson(fetch, postinumerotUrl);
+export const kunnat = fetch => fetchJson(fetch, kunnatUrl);
