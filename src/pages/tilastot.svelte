5bcea623f1ec13fad237488b13fb1a7223987171
Pasi Semykine <23694709+pasisem@users.noreply.github.com>
2021-12-23 12:56:47 +0200

    Add scrollTo for better home page link transition

--- src/pages/tilastot.svelte
+++ src/pages/tilastot.svelte
@@ -176,6 +176,14 @@
   window.onafterprint = () => {
     printing = false;
   };
+
+  onMount(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  });
 </script>
 
 <style>
