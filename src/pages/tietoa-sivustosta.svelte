050edcca29c8904b95c9131dcdeca92c9d0c930b
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2023-01-23 10:33:20 +0200

    Copy saavutettavuusselosta -> tietoa-sivustosta

---
+++ src/pages/tietoa-sivustosta.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { onMount } from 'svelte';
+  import Container, { styles as containerStyles } from '@Component/container';
+  import { _, locale } from '@Localization/localization';
+  import Seo from '@Component/seo';
+
+  import PdfFi from '@Asset/Saavutettavuus.pdf';
+  import PdfSv from '@Asset/Saavutettavuus.pdf';
+
+  let component;
+
+  onMount(() => {
+    component?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  });
+</script>
+
+<Seo
+  title="{$_('ENERGIATODISTUSREKISTERI')} - {$_('SAAVUTETTAVUUSSELOSTE')}"
+  descriptionFi={$locale == 'fi' ? $_('SAAVUTETTAVUUSSELOSTE') : undefined}
+  descriptionSv={$locale == 'sv' ? $_('SAAVUTETTAVUUSSELOSTE') : undefined} />
+
+<Container {...containerStyles.white}>
+  <div class="px-4 lg:px-8 xl:px-16 pt-8 pb-4 mx-auto" bind:this={component}>
+    <div>
+      <h1 class="mb-4">{$_('SAAVUTETTAVUUSSELOSTE_TIETOA')}</h1>
+      {#if $locale == 'sv'}
+        <a href={PdfSv} target="_blank" class="text-darkgreen">
+          {$_('SAAVUTETTAVUUSSELOSTE_LINK_LABEL')}
+        </a>
+      {:else}
+        <a href={PdfFi} target="_blank" class="text-darkgreen">
+          {$_('SAAVUTETTAVUUSSELOSTE_LINK_LABEL')}
+        </a>
+      {/if}
+    </div>
+  </div>
+</Container>
