cd813bb42906567822618f460e1751c0605b6940
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2023-02-15 15:53:04 +0200

    AE-1823 Instructions in Swedish

---
+++ src/pages/aineistopalvelu-liittyminen.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { _, locale } from '@Localization/localization';
+  import AineistopalveluLiittyminenFi from '@Page/aineistopalvelu-liittyminen-fi';
+  import AineistopalveluLiittyminenSv from '@Page/aineistopalvelu-liittyminen-sv';
+</script>
+
+{#if $locale === 'sv'}
+  <AineistopalveluLiittyminenSv />
+{:else}
+  <AineistopalveluLiittyminenFi />
+{/if}
--- src/router/router.svelte
+++ src/router/router.svelte
@@ -24,7 +24,7 @@
   import TietoaSivustosta from '@Page/tietoa-sivustosta';
   import LaatijanKirjautuminen from '@Page/laatijankirjautuminen';
   import Rekisteroitymisohjeet from '@Page/rekisteroitymisohjeet';
-  import AineistopalveluLiittyminenFi from '@Page/aineistopalvelu-liittyminen-fi';
+  import AineistopalveluLiittyminen from '@Page/aineistopalvelu-liittyminen';
   import Tilastot from '@Page/tilastot';
   import NotFound from '@Page/not-found';
 
@@ -61,7 +61,7 @@
     page = Rekisteroitymisohjeet;
   });
   router('/aineistopalvelu-liittyminen', () => {
-    page = AineistopalveluLiittyminenFi;
+    page = AineistopalveluLiittyminen;
   });
   router('/tilastot', () => {
     page = Tilastot;
